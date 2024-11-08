f7b54b066f2d49c13dc69321ad39297fcd56a6bb
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2024-11-08 15:49:06 +0900

    [level 2] Title: 타겟 넘버, Time: 8.40 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <stack>
 using namespace std;
 
 int result = 0;
@@ -21,24 +22,42 @@ void dfs(vector<int>& _vec, int _ref, int value, int target) {
 
 int solution(vector<int> numbers, int target) {
     result = 0;
-
+    int dfsresult = 0;
     std::queue<std::pair<int, int>> qu;
+    std::stack<std::pair<int, int>> st;
     std::pair<int, int> pa;
     qu.push(std::make_pair(numbers[0], 0));
     qu.push(std::make_pair(-numbers[0], 0));
 
-    while(!qu.empty()){
-        pa = qu.front();
-        qu.pop();
+    st.push(std::make_pair(numbers[0], 0));
+    st.push(std::make_pair(-numbers[0], 0));
+
+    //while(!qu.empty()){
+    //    pa = qu.front();
+    //    qu.pop();
+    //    if (pa.second == numbers.size() - 1) {
+    //        if (pa.first == target) {
+    //            ++result;
+    //        }
+    //        continue;
+    //    }
+    //    qu.push(std::make_pair(numbers[pa.second + 1] + pa.first, pa.second + 1));
+    //    qu.push(std::make_pair(- numbers[pa.second + 1] + pa.first, pa.second + 1));
+    //}
+
+    while (!st.empty()) {
+        pa = st.top();
+        st.pop();
         if (pa.second == numbers.size() - 1) {
             if (pa.first == target) {
                 ++result;
             }
             continue;
         }
-        qu.push(std::make_pair(numbers[pa.second + 1] + pa.first, pa.second + 1));
-        qu.push(std::make_pair(- numbers[pa.second + 1] + pa.first, pa.second + 1));
+        st.push(std::make_pair(numbers[pa.second + 1] + pa.first, pa.second + 1));
+        st.push(std::make_pair(-numbers[pa.second + 1] + pa.first, pa.second + 1));
     }
+
 
     return result;
 }
