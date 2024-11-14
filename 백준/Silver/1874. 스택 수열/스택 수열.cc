9ca27b10b23d571d0fbce8c4dd1c40223055bbf3
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2024-11-14 16:42:07 +0900

    [Silver II] Title: 스택 수열, Time: 108 ms, Memory: 7056 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,51 +1,50 @@
-#include <iostream>
-#include <string>
-#include <stack>
-#include <vector>
 #include <set>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
 using namespace std;
+
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+	int index, num;
+	cin >> index;
+	int value = 0;
 	std::stack<int> st;
-	std::vector<char> vec;
 	std::set<int> se;
-	int Count = 0;
-	cin >> Count;
-	int num = 0;
-	int temp = 0;
-	while (Count--) {
+	std::vector<char> vec;
+	while (index--) {
 		cin >> num;
-		if (num > temp) {
-			while (num > temp) {
-				if (se.find(++temp) != se.end()) {
-					continue;
-				}
-				st.push(temp);
+		while (value < num) {
+			if (se.find(value + 1) == se.end()) {
+				st.push(++value);
+				se.insert(value);
 				vec.push_back('+');
-				se.insert(temp);
 			}
-			vec.push_back('-');
-			st.pop();
-			continue;
+			else {
+				++value;
+			}
 		}
-		else {
-			while (num < temp) {
-				if (st.top() != num) {
-					std::cout << "NO";
-					return 0;
-				}
-				else {
-					temp = num;
-					vec.push_back('-');
-					st.pop();
-				}
+		while (value >= num) {
+			if (st.top() != num) {
+				std::cout << "NO";
+				return 0;
+			}
+			else {
+				st.pop();
+				vec.push_back('-');
+				value = num - 1;
 			}
 		}
 
 	}
+
 	for (char c : vec) {
 		std::cout << c << '\n';
 	}
+
+	return 0;
 }
