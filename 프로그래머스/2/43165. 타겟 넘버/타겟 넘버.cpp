4e042f5d98c3f9e127ae0075a3000f79decab06a
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2024-11-19 00:42:04 +0900

    [level 2] Title: 타겟 넘버, Time: 13.18 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,58 +1,38 @@
 #include <iostream>
-#include <string>
-#include <regex>
-#include <queue>
+#include <vector>
+#include <algorithm>
 #include <stack>
+#include <queue>
 using namespace std;
 
-int bfs(vector<int>& numbers, int target) {
-	int numsize = numbers.size();
-	std::queue<std::pair<int, int>> qu;
-	qu.push(make_pair(numbers[0], 0));
-	qu.push(make_pair(-numbers[0], 0));
-	std::pair<int, int> pqu;
-	int iqu = 0;
-	while (!qu.empty()) {
-		pqu = qu.front();
-		qu.pop();
-		if (pqu.second + 1 < numsize) {
-			qu.push(make_pair(pqu.first + numbers[pqu.second + 1], pqu.second + 1));
-			qu.push(make_pair(pqu.first - numbers[pqu.second + 1], pqu.second + 1));
-		}
-		else {
-			if (target == pqu.first) {
-				++iqu;
-			}
-		}
-	}
-	return iqu;
-}
-
-int dfs(vector<int>& numbers, int target) {
-	int numsize = numbers.size();
-	std::stack<std::pair<int, int>> st;
-	st.push(make_pair(numbers[0], 0));
-	st.push(make_pair(-numbers[0], 0));
-	std::pair<int, int> pst;
-	int ist = 0;
-	while (!st.empty()) {
-		pst = st.top();
-		st.pop();
-		if (pst.second + 1 < numsize) {
-			st.push(make_pair(pst.first + numbers[pst.second + 1], pst.second + 1));
-			st.push(make_pair(pst.first - numbers[pst.second + 1], pst.second + 1));
-		}
-		else {
-			if (target == pst.first) {
-				++ist;
-			}
-		}
-	}
-	return ist;
-}
 
 
 int solution(vector<int> numbers, int target) {
 
-	return bfs(numbers, target);
+	int result = 0;
+	std::pair<int, int> pa;
+	int numsize = numbers.size();
+	std::queue<std::pair<int, int>> qu;
+	int n = 0;
+	//지금 총합, 지금 몇 번째까지 더했냐
+	qu.push(std::make_pair(n, numbers[n]));
+	qu.push(std::make_pair(n, -numbers[n]));
+
+	while (!qu.empty()) {
+		pa = qu.front();
+		qu.pop();
+		n = pa.first;
+		if (n == numsize - 1) {
+			if (pa.second == target) {
+				++result;
+			}
+			continue;
+		}
+		qu.push(std::make_pair(n + 1, pa.second + numbers[n + 1]));  // 1, 1  (1  -1)
+		qu.push(std::make_pair(n + 1, pa.second - numbers[n + 1]));
+		// 1 1  1 -1  -1 1   -1 -1  2 0 0 -2
+	}
+
+	return result;
 }
+
