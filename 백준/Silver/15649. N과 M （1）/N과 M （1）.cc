cc129b7ef1bb17ea7ea306899480fbf71a3168bf
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2024-12-11 00:11:18 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,36 +1,35 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int n, m;
 int arr[10];
-bool isused[10];
+bool check[10];
 
+int N, M;
 
 
 void fun(int _n) {
+	if (_n == M) {
+		for (int i = 0; i < M; ++i) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << '\n';
+	}
 
-    if (_n == m) {
-        for (int i = 0; i < m; ++i) {
-            std::cout << arr[i] << ' ';
-        }
-        std::cout << '\n';
-    }
-    else {
-        for (int i = 1; i <= n; ++i) {
-            if (isused[i] != true) {
-                arr[_n] = i;
-                isused[i] = true;
-                fun(_n + 1);
-                isused[i] = false;
-            }
-        }
-    }
+	for (int i = 1; i <= N; ++i) {
+		if (check[i] == false) {
+			arr[_n] = i;
+			check[i] = true;
+			fun(_n + 1);
+			check[i] = false;
+		}
+	}
 }
 
 
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> m;
-    fun(0);
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	std::cin >> N >> M;
+	fun(0);
 }
