b554537a483fc928b09f2a274a8d15e76a908087
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-06-30 02:23:44 +0900

    Time: 0 ms (100%), Space: 12.4 MB (38.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
-	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-		std::vector<int> result;
-
-		for (int i = 0; i < m; ++i) {
-			result.push_back(nums1[i]);
-		}
-
-		for (int i = 0; i < n; ++i) {
-			result.push_back(nums2[i]);
-		}
-
-		std::sort(result.begin(), result.end());
-        nums1 = result;
-	}
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+                std::vector<int> vec;
+        std::merge(nums1.begin(), nums1.begin() + m, nums2.begin(), nums2.begin() + n, std::back_inserter(vec));
+        nums1 = vec;
+    }
 };
