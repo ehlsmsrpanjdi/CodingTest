c2b2ed873455880e66d6b4f2e4d1fd17ae64678d
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-06-30 02:33:20 +0900

    Time: 0 ms (100%), Space: 12.2 MB (95.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,8 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-                std::vector<int> vec;
-        std::merge(nums1.begin(), nums1.begin() + m, nums2.begin(), nums2.begin() + n, std::back_inserter(vec));
-        nums1 = vec;
+        int oneLastPointer = m - 1;
+        int twoLastPointer = n - 1;
+        int totalLastPointer = m + n - 1;
+
+        while (oneLastPointer >= 0 && twoLastPointer >= 0) {
+            if (nums1[oneLastPointer] > nums2[twoLastPointer]) {
+                nums1[totalLastPointer] = nums1[oneLastPointer];
+                --totalLastPointer;
+                --oneLastPointer;
+            }
+            else {
+                nums1[totalLastPointer] = nums2[twoLastPointer];
+                --totalLastPointer;
+                --twoLastPointer;
+            }
+        }
+
+        while (twoLastPointer >= 0) {
+            nums1[totalLastPointer] = nums2[twoLastPointer];
+            --totalLastPointer;
+            --twoLastPointer;
+        }
+
+
+
     }
 };
