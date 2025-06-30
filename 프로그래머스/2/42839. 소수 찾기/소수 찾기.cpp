bf8ef65f4acc75e3001bd3c6f6fb77f6fba35d25
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-06-30 21:37:13 +0900

    [level 2] Title: 소수 찾기, Time: 0.88 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <queue>
 #include <vector>
 #include <algorithm>
 #include <iterator>
@@ -6,15 +7,16 @@
 #include <iostream>
 
 using namespace std;
-unordered_set<int> unset;
-int numberSize = 0;
-std::vector<bool> isVisited;
-string useValue;
+
+
+std::vector<bool> isUsed;
+std::unordered_set<int> unSet;
 
 bool IsPrime(int _Value) {
-	if (_Value == 1 || _Value == 0) {
+	if (0 == _Value || 1 == _Value) {
 		return false;
 	}
+
 	for (int i = 2; i * i <= _Value; ++i) {
 		if (_Value % i == 0) {
 			return false;
@@ -23,38 +25,29 @@ bool IsPrime(int _Value) {
 	return true;
 }
 
-void BackTracking(string current) {
-	if (!current.empty()) {
-		int num = stoi(current);
-		if (true == IsPrime(num)) {
-			unset.insert(num);
+void BackTracking(string current, const string& _str) {
+
+	if (0 != current.size()) {
+		int Value = std::stoi(current);
+		if (true == IsPrime(Value)) {
+			unSet.insert(Value);
 		}
 	}
 
-	for (int i = 0; i < numberSize; ++i) {
-		if (false == isVisited[i]) {
-			isVisited[i] = true;
-			BackTracking(current + useValue[i]);
-			isVisited[i] = false;
+
+	for (int i = 0; i < isUsed.size(); ++i) {
+		if (false == isUsed[i]) {
+			isUsed[i] = true;
+			BackTracking(current + _str[i], _str);
+			isUsed[i] = false;
 		}
 	}
 }
 
-
 int solution(string numbers) {
 	int answer = 0;
+	isUsed.resize(numbers.size(), false);
+	BackTracking("", numbers);
 
-
-	numberSize = numbers.size();
-
-
-	useValue = numbers;
-	isVisited.resize(numberSize, false);
-
-	BackTracking("");
-	for (int value : unset) {
-		std::cout << value << std::endl;
-	}
-
-	return unset.size();
+	return unSet.size();
 }
