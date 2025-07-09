58a8c113b77ed58bf95f77b422999b71493ed5f7
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-07-10 08:09:04 +0900

    Time: 2 ms (54.76%), Space: 8 MB (37.39%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int firstBadVersion(int n) {
-        int left = 0;
-        int right = n;
+	int firstBadVersion(int n) {
+		int left = 0;
+		int right = n;
 
+		while (left < right) {
 
-        while (left < right) {
-        int middle = left + (right - left) / 2;
-            if (isBadVersion(middle) == true) {
-                right = middle;
-            }
-            else {
-                left = middle + 1;
-            }
-        }
-        return left;
+			int middle = left + (right - left) / 2;
 
-    }
+			if (isBadVersion(middle) == true) {
+				right = middle;
+			}
+			else {
+				left = middle + 1;
+			}
+		}
+		return left;
+	}
 };
