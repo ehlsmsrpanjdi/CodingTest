2eb56bf4bd846a422e51490cc3b81d38cc7e7829
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-07-09 16:18:53 +0900

    Time: 0 ms (100%), Space: 7.8 MB (69.51%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+
+
+        while (left < right) {
+        int middle = left + (right - left) / 2;
+            if (isBadVersion(middle) == true) {
+                right = middle;
+            }
+            else {
+                left = middle + 1;
+            }
+        }
+        return left;
+
+    }
+};
