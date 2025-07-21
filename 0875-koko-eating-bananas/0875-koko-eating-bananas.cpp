b0309695590b915a5e139f981e0c23a04fff2ef3
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-07-22 08:20:20 +0900

    Time: 7 ms (73.31%), Space: 23 MB (42.11%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,30 +1,26 @@
 bool IsPossible(std::vector<int>& piles, int h, int mid) {
 	int total = 0;
 	for (int i : piles) {
-		if (i % mid == 0) {
-			total += i / mid;
-		}
-		else {
-			total += i / mid + 1;
-		}
+		total += (i + mid - 1) / mid;
 		if (total > h) {
 			return false;
 		}
 	}
-
 	return true;
 }
 
 class Solution {
 public:
 	int minEatingSpeed(vector<int>& piles, int h) {
-		std::sort(piles.begin(), piles.end());
 
 		int left = 1;
-		int right = piles.back();
+
+		int right = *std::max_element(piles.begin(), piles.end());
+
 
 		while (left <= right) {
 			int mid = left + (right - left) / 2;
+
 			if (IsPossible(piles, h, mid) == true) {
 				right = mid - 1;
 			}
@@ -32,6 +28,8 @@ public:
 				left = mid + 1;
 			}
 		}
+
 		return left;
+
 	}
 };
