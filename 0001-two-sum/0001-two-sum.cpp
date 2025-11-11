a8b5a0ae94490acc0bcbef7ef295c2b9a2bb9ccd
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-11-11 18:30:21 +0900

    Time: 3 ms (68.76%), Space: 14.9 MB (36.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {  //2, 7
-		std::unordered_map<int, int> map;
-		std::vector<int> list;
-		for (int i = 0; i < nums.size(); ++i) {
-			int com = target - nums[i];
-			if (map.contains(com) == true) {
-				list = { map[com], i };
-				return list;
-			}
-			map[nums[i]] = i;
-		}
+	vector<int> twoSum(vector<int>& nums, int target) {
+		std::unordered_map<int, int> unMap;
 
-		return list;
+		std::unordered_map<int, int>::iterator iter;
+
+		std::vector<int> vec;
+
+		for (int i = 0; i < nums.size(); ++i) {
+
+			iter = unMap.find(nums[i]);
+			if (unMap.end() != iter && (*iter).second != i) {
+				vec.push_back((*iter).second);
+				vec.push_back(i);
+				return vec;
+			}
+
+			unMap[target - nums[i]] = i;
+		}
+		return vec;
 	}
 };
