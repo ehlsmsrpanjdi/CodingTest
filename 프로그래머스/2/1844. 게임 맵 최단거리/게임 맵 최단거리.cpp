18324cfc62884a84bfa5f68888b60077247792c6
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-11-14 17:09:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.32 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,53 +1,56 @@
-#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
 #include <queue>
+#include <stack>
+#include <iostream>
+
 using namespace std;
 
+bool isvisited[101][101];
+int distanceMap[101][101];
 
-int dist[101][101];
-bool visit[101][101];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
 
 int solution(vector<vector<int>> maps)
 {
+	int answer = 0;
+
 	std::queue<std::pair<int, int>> qu;
-	int n, m;
-	m = maps.size();  //y값이자 도착지
-	n = maps[0].size();	//x값이자 도착지
 
-	int dx[4] = { 1,0,-1,0 };
-	int dy[4] = { 0,1,0,-1 };
+	qu.push(std::make_pair(0, 0));
+	distanceMap[0][0] = 1;
 
-	qu.push(make_pair(0, 0));
-	visit[0][0] = true;
-	dist[0][0] = 1;
+	std::pair<int, int> pa;
+	while (!qu.empty()) {
+		pa = qu.front();
 
-	while (!qu.empty()) {		//qu가 비어있지 않을 때 까지
-		std::pair<int, int> pa = qu.front();
-
-		qu.pop();
-
-		if (pa.first == n - 1 && pa.second == m - 1) {
-			return dist[pa.second][pa.first];
-			//정답
+		if (pa.first == maps.size()-1 && pa.second == maps[0].size()-1) {
+			return distanceMap[pa.first][pa.second];
 		}
 
-		int x = pa.first;
-		int y = pa.second;
-
-		int tempx;
-		int tempy;
-
+		qu.pop();
 		for (int i = 0; i < 4; ++i) {
-			tempx = x + dx[i];
-			tempy = y + dy[i];
-
-			if (tempx >= 0 && tempy >= 0 && tempx < n && tempy < m && (visit[tempy][tempx] == false)
-				&& maps[tempy][tempx] == 1) {
-				dist[tempy][tempx] = dist[y][x] + 1;
-				visit[tempy][tempx] = true;
-				qu.push(make_pair(tempx, tempy));
+			int tempx = pa.first + dx[i];
+			int tempy = pa.second + dy[i];
+			if (tempx >= 0 && tempx < maps.size() && tempy >= 0 && tempy < maps[0].size())
+			{
+				if (maps[tempx][tempy] == 1) {
+					if (isvisited[tempx][tempy] == false) {
+						isvisited[tempx][tempy] = true;
+						qu.push(make_pair(tempx, tempy));
+						distanceMap[tempx][tempy] = distanceMap[pa.first][pa.second] + 1;
+					}
+				}
 			}
 		}
 	}
-	return -1;
 
+	return -1;
 }
+
