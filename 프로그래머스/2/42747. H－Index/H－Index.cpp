16ec125d26c048409d6edb13866c4c07456476aa
ehlsmsrpanjdi <90026344+ehlsmsrpanjdi@users.noreply.github.com>
2025-11-20 04:34:42 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,29 +1,30 @@
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
 #include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <iostream>
+
+
 using namespace std;
 
-
-// 1,1,1,1,2
-
 int solution(vector<int> citations) {
-    int answer = 0;
+	int answer = 0;
 
-    std::sort(citations.begin(), citations.end(), std::greater<int>());
+	std::sort(citations.begin(), citations.end(), greater<int>());
 
-    int maxvalue = 0;
-    int maxSize = citations.size();
-
-    for (int i = 0; i < maxSize; ++i) {
-        if (citations[i] >= i + 1) {
-            maxvalue = i + 1;
-        }
-        else {
-            break;
-        }
-    }
- 
+	for (int i = 0; i < citations.size(); ++i) {
+		//  논문 인용횟수 >= 지금까지 본 논문개수
+		if (citations[i] >= i + 1) {
+			answer = i + 1;
+		}
+		else break;
+	}
 
 
-    return maxvalue;
+	return answer;
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?gad_source=1&gad_campaignid=22199869887&gbraid=0AAAAAC_c4nCEMghFjix506SmOLONuiUyd&gclid=Cj0KCQjwpf7CBhCfARIsANIETVoGIfuep-pkJLb93gZofk76Cw_LTUD2TfT4P62W2fFAbDxbn-yK6zAaAkiGEALw_wcB) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 3.67 MB, 시간: 0.05 ms
+메모리: 4.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 29일 06:04:23
+2025년 11월 20일 04:34:38
 
 ### 문제 설명
 
